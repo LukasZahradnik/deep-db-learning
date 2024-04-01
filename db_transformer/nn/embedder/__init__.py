d57f3643b564fc97089047e237ffc2f9f6fabb81
Jakub Peleska <pelesjak@fel.cvut.cz>
2024-04-01 20:20:25 +0200

    Temporarily move tabular embedders

--- db_transformer/nn/embedder/__init__.py
+++ db_transformer/nn/embedder/__init__.py
@@ -1,9 +1,6 @@
 from .columns import *
-from .embedders import *
 from .stypes import *
-from .db_embedder import *
+from .table import *
 
-from .excelformer_embedder import *
-from .fttransformer_embedder import *
-from .tabtransformer_embedder import *
-from .trompt_embedder import *
+from .embedders import *
+from .db_embedder import *
---
+++ db_transformer/nn/embedder/table/__init__.py
@@ -0,0 +1,4 @@
+from .excelformer_embedder import *
+from .fttransformer_embedder import *
+from .tabtransformer_embedder import *
+from .trompt_embedder import *
